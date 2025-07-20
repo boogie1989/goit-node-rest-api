e4d09703864a9d9a81c2de6e6aa6a63f0b28e900
boogie1989 <serhiibuhai.work@gmail.com>
2025-07-20 19:52:42 +0300

    fixed start

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { createApp } from "./createApp.js";
 
-const app = createApp();
-app.listen(3002, () => {
+const app = await createApp();
+app.listen(3000, () => {
     console.log("Server is running. Use our API on port: 3000");
 });
